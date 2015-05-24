fe8ff504331ad56d5ca49315347080fec944b263
Dan McCormack <dmccormack22@gmail.com>
2015-05-24 14:18:36 -0400

    Abstract Processor included

--- src/main/groovy/org/anc/processor/xml/XMLProcessor.groovy
+++ src/main/groovy/org/anc/processor/xml/XMLProcessor.groovy
@@ -2,6 +2,7 @@ package org.anc.processor.xml
 
 import org.anc.index.core.IndexImpl
 import org.xces.graf.io.dom.ResourceHeader
+import org.anc.processor.Abstract.AbstractProcessor
 
 import javax.ws.rs.Path
 
@@ -13,9 +14,10 @@ class XMLProcessor extends AbstractProcessor{
 
 
     public XMLProcessor() {
+        super(["f.penn", "f.sentences", "f.biber", "f.c5", "f.c7", "f.cb",
+               "f.content", "f.event", "f.hepple", "f.logical",
+               "f.mpqa", "f.nc", "f.ne", "f.none","f.slate_coref",
+               "f.vc", "f.ptb", "f.ptbtok", "f.fn", "f.fntok", "f.s"] as List<String>)
         processor = new org.anc.tool.xml.XMLProcessor();
-        //TODO This path should not be hard coded.
-        header = new ResourceHeader(new File("/var/corpora/MASC-3.0.0/resource-header.xml"))
-        index = new IndexImpl().loadMasc3Index()
     }
 }
--- src/test/groovy/org/anc/processor/xml/ProcessorTest.groovy
+++ src/test/groovy/org/anc/processor/xml/ProcessorTest.groovy
@@ -3,6 +3,10 @@ package org.anc.processor.xml
 import org.anc.processor.xml.i18n.Messages
 import org.junit.*
 
+import javax.ws.rs.core.Response
+
+import static org.junit.Assert.*
+
 /**
  * Created by danmccormack on 12/12/14.
  */
@@ -21,4 +25,80 @@ class ProcessorTest {
         processor = null
     }
 
+    /**
+     * Cases to test: passing (1, all, a few)
+     *                failing (none, one wrong in a list of right, all wrong, capital)
+     */
+    @Test
+    void testValidAnnotations()
+    {
+        //PASSING
+
+        //ONE
+        def pass1 = ["f.penn"] as List<String>
+        assertTrue(processor.validAnnotations(pass1))
+
+        //FEW
+        def pass2 = ["f.penn", "f.sentences"] as List<String>
+        assertTrue(processor.validAnnotations(pass2))
+
+
+        //FAILING
+        def fail2 = ["thing1", "thing2", "red", "blue"] as List<String>
+        assertFalse(processor.validAnnotations(fail2))
+
+        //ONE WRONG IN LIST OF RIGHT
+        def fail3 = ["penn", "f.cb", "f.penn", "f.ne"] as List<String>
+        assertFalse(processor.validAnnotations(fail3))
+
+        //Capitals
+        def fail4 = ["F.PENN"] as List<String>
+        assertFalse(processor.validAnnotations(fail4))
+
+    }
+    @Test
+    /**
+     *  Cases to test: empty string, one word, two words, n-words
+     */
+    void testParseAnnotations()
+    {
+        //EMPTY STRING
+        // The empty string should result in ALL annotation types being selected
+        assertTrue("Empty string should return all acceptable annotations.", processor.parseAnnotations("") == processor.Acceptable.toList())
+
+        //ONE WORD
+        List expected = ["f.penn"]
+        List actual = processor.parseAnnotations("penn")
+        assertTrue "Actual is " + actual, actual == expected
+
+        //TWO WORDS
+        expected = ["f.penn", "f.cb"]
+        assertTrue(processor.parseAnnotations("penn,cb") == expected)
+
+        //N WORDS
+        // Use Groovy range operator to populate the collection, then add the 'f.' prefix.
+        expected = (1..9).collect { "f." + it }
+        actual = processor.parseAnnotations("1,2,3,4,5,6,7,8,9")
+        assertTrue("Expected is " + expected + " Actual is " + actual, actual == expected)
+    }
+
+    @Test
+    void testProcess() {
+        Response response = processor.process("penn, s", "MASC3-0202")
+        assertTrue response.entity, response.status == 200
+    }
+
+    @Test
+    void testInvalidDocId() {
+        Response response = processor.process("penn, s", "Invalid ID")
+        assertTrue response.entity, response.status == 500
+        assertTrue "Wrong error message returned", response.entity == MESSAGES.INVALID_ID
+    }
+
+    @Test
+    void testInvalidAnnotationType() {
+        Response response = processor.process("invalid,annotations", "MASC3-0202")
+        assertTrue response.entity, response.status == 500
+        assertTrue "Wrong error message returned.", response.entity == MESSAGES.INVALID_TYPE
+    }
 }
