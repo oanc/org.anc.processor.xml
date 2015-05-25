0aaf75cbb89ea5f333a047526ce0bfad2b6664a0
Keith Suderman <suderman@anc.org>
2015-05-25 10:48:48 -0400

    Changed version for the XMLProcessor imported.

--- src/main/groovy/org/anc/processor/xml/XMLProcessor.groovy
+++ src/main/groovy/org/anc/processor/xml/XMLProcessor.groovy
@@ -1,6 +1,7 @@
 package org.anc.processor.xml
 
 import org.anc.index.core.IndexImpl
+import org.anc.tool.api.IProcessor
 import org.xces.graf.io.dom.ResourceHeader
 import org.anc.processor.Abstract.AbstractProcessor
 
@@ -18,6 +19,9 @@ class XMLProcessor extends AbstractProcessor{
                "f.content", "f.event", "f.hepple", "f.logical",
                "f.mpqa", "f.nc", "f.ne", "f.none","f.slate_coref",
                "f.vc", "f.ptb", "f.ptbtok", "f.fn", "f.fntok", "f.s"] as List<String>)
-        processor = new org.anc.tool.xml.XMLProcessor();
+
+        processor = new org.anc.tool.xml.XMLProcessor()
+        processor.setOptions("Discard")
+        //println processor.options
     }
 }
